8b4985e4b1bcca14ce638006c17a02f2f4204f61
juanaotermin19 <142274725+juanaotermin19@users.noreply.github.com>
2024-05-05 16:59:10 -0300

    Update Entrega 1.py

--- Entrega 1.py
+++ Entrega 1.py
@@ -2,4 +2,21 @@ import pandas as pd
 from inline_sql import sql, sql_val
 
 
+tabla_sedes = "/Users/juanaotermin/Documents/Facultad Exactas/Laboratorio de Datos/TP_01/lista-sedes.csv"
+
+df = pd.read_csv(tabla_sedes, index_col= 'sede_id')
+
+#Voy a eliminar los campos de mi tabla donde el estado sea "Inactivo"
+
+for x in df.index: 
+  if df.loc[x, "estado"] == 'Inactivo':
+    df.drop(x, inplace = True)
+
+
+print(df.info())
+
+#Como se que son todas sedes activas, puedo eliminar la columna "estado"
+#Al mismo tiempor quiero eliminar la columna de "sede_tipo" ya que no es relevante para mi objetivo
+
+df_limpio = df.drop(["estado", "sede_tipo"], axis = 1)
 #%%
