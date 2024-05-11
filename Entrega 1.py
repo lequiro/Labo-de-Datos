501617aec7625616839e530c172b41ea79b16336
juanaotermin19 <142274725+juanaotermin19@users.noreply.github.com>
2024-05-11 16:41:32 -0300

    Primeras Consultas en SQL

--- Entrega 1.py
+++ Entrega 1.py
@@ -152,3 +152,129 @@ for x in SEDES.index:
 #Al mismo tiempo quiero eliminar la columna de "sede_tipo" ya que no es relevante para mi objetivo
 SEDES = SEDES[['sede_id','pais_iso_3','sede_tipo']]
 SEDES = SEDES.rename(columns = {'pais_iso_3' : 'iso3'})
+
+#%%% CONSULTAS SQL
+
+
+"""
+PUNTO I)
+Para cada país informar cantidad de sedes, cantidad de secciones en promedio que poseen 
+sus sedes y el flujo monetario neto de Inversión Extranjera Directa (IED) del país en 
+el año 2022. El orden del reporte debe respetar la cantidad de sedes (de manera 
+descendente). En caso de empate, ordenar alfabéticamente por nombre de país
+
+"""
+#Cuento cantidad de sedes por pais
+consulta1 = """
+               SELECT p.iso3, COUNT(s.sede_id) AS cantidad_sedes,
+               FROM PAISES AS p
+               LEFT OUTER JOIN SEDES AS s ON p.iso3 = s.iso3
+               GROUP BY p.iso3;
+
+              """
+              
+tablaContadorSedes = sql^ consulta1
+#Agrego resultado anterior a la tabla
+consulta2 = """
+               SELECT DISTINCT p.nombre, t.cantidad_sedes, p.iso3
+               FROM PAISES AS p
+                JOIN tablaContadorSedes AS t
+               ON p.iso3 = t.iso3
+              """
+
+tablaPaisesYSedes = sql^ consulta2
+
+#Junto tabla secciones y tabla sedes
+
+consulta3 = """
+   SELECT DISTINCT sec.tipo_seccion, sed.iso3, sed.sede_id,
+   FROM SEDES AS sed
+   LEFT JOIN SECCIONES AS sec
+   ON sed.sede_id = sec.sede_id
+    
+"""
+tablaSeccionesYSedes = sql^ consulta3
+#Cuento cantidad de secciones por sede_id (cuento las veces que se repiten sede_id, ya que si sede_id se repite 2 veces significa que tiene dos secciones)
+#FALTA CALCULAR EL PROMEDIO
+consulta5 = """
+            SELECT DISTINCT COUNT(sede_id) AS cantidad_secciones, sede_id, ANY_VALUE(iso3) AS iso3
+            FROM tablaSeccionesYSedes 
+            GROUP BY sede_id
+
+    """
+tablaCantidadSeccionesPorSede = sql^ consulta5
+
+consulta4 = """
+    SELECT ROUND(AVG(t.cantidad_secciones),2) AS promedio_secciones, ANY_VALUE(t.sede_id) AS sede_id, t.iso3
+    FROM tablaCantidadSeccionesPorSede as t
+    GROUP BY iso3
+"""
+tablaPromedioSeccionesPorSede = sql^ consulta4
+
+consulta6 = """
+        SELECT tc.sede_id, tp.cantidad_sedes, tc.promedio_secciones, tp.nombre, tp.iso3
+        FROM tablaPaisesYSedes AS tp
+        LEFT JOIN  tablaPromedioSeccionesPorSede AS tc
+        ON tc.iso3 = tp.iso3
+"""
+tablaCantidadSedesYSeccionesPorPais = sql^ consulta6
+#calculo el flujo en 2022 por pais
+#PODRIAMOS AGREGAR EN QUE SE ESTA MIDIENDO EL FLUJO (USD$??)
+consulta7 = """
+    SELECT monto AS IED_2022, iso3
+    FROM FLUJOS_MONETARIOS
+    WHERE fecha == '2022'
+"""
+
+tablaIED2022 = sql^consulta7
+
+#Uno este resultado con los resultados anteriores de cant de sedes por pais y secciones por sede y ordeno 
+
+consulta8 = """
+    SELECT tc.cantidad_sedes AS sedes, tc.nombre AS pais, ti.IED_2022, tc.promedio_secciones AS seccion
+    FROM tablaCantidadSedesYSeccionesPorPais as tc
+    LEFT JOIN tablaIED2022 as ti
+    ON tc.iso3 = ti.iso3
+    ORDER BY tc.cantidad_sedes DESC, tc.nombre
+"""
+tablaResultado1 = sql^ consulta8
+#FALTA REEMPLAZAR LOS NULLS POR 0 SI ES FLOAT O "-" SI SE ESPERABA UN STRING
+        
+"""
+III) Para saber cuál es la vía de comunicación de las sedes en cada país, nos
+hacemos la siguiente pregunta: ¿Cuán variado es, en cada el país, el tipo de 
+redes sociales que utilizan las sedes? Se espera como respuesta que para cada país 
+se informe la cantidad de tipos de redes distintas utilizadas. Por ejemplo, si en Chile u
+tilizan 4 redes de facebook, 5 de instagram y 4 de twitter, el valor para Chile debería ser 3 
+(facebook, instagram y twitter).
+"""
+#armo tabla de sedes con red social para lograr tener la foreign key iso3
+
+consulta9 = """
+    SELECT rs.contacto, rs.tipo_red, s.iso3
+    FROM REDES_SOCIALES AS rs
+    LEFT JOIN SEDES as s
+    ON s.sede_id = rs.sede_id
+"""
+
+tablaSedeYRedSocial = sql^consulta9
+
+#luego conecto el resultado anterior con la tabla paises
+consulta10 = """
+    SELECT *
+    FROM PAISES as p
+    LEFT OUTER JOIN tablaSedeYRedSocial as t
+    ON p.iso3 = t.iso3
+"""
+tablaPaisesYRedes = sql ^ consulta10
+
+#cuento la cantidad de tipo_red para cada pais, uso el distinct para que me cuente solo los que son distintos
+
+
+consulta11 = """
+        SELECT COUNT(DISTINCT tipo_red) AS cantidad_tipos, iso3, ANY_VALUE(nombre) AS pais
+        FROM tablaPaisesYRedes
+        GROUP BY iso3
+"""
+
+tablaCantidadRedPorPais = sql ^ consulta11
